620ada8a7ddbf618322bf44bd6fb6ddb55f163b5
Jisub Shim <jisub5322@naver.com>
2024-03-16 22:53:31 +0900

    [실버4] 괄호

---
+++ BaekJoon/Silver/9012_괄호.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	int* result = new int[T];
+	for (int i = 0; i < T; i++) {
+		string ps;
+		cin >> ps;
+
+		stack<char> st;
+		bool isValid = true;
+		for (char c : ps) {
+			if (c == '(')
+				st.push(c);
+			else if (c == ')') {
+				if (st.empty() || st.top() != '(') {
+					isValid = false;
+					break;
+				}
+				st.pop();
+			}
+		}
+
+		if (!st.empty() || !isValid)
+			result[i] = 0;
+		else
+			result[i] = 1;
+	}
+	for (int i = 0; i < T; i++) {
+		if (result[i] == 0)
+			cout << "NO" << endl;
+		else
+			cout << "YES" << endl;
+	}
+
+	return 0;
+}
